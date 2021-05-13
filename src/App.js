da3bb378e5ef170e788784128d1184e54ee2725c
bharat2913 <bs987143@gmail.com>
2021-05-13 14:15:02 +0530

    feat: added homepage(footer, accodion, faq's)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
-// import Jumbotron from './components/jumbotron';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, Browse, Signin, Signup } from './pages';
+
+import * as ROUTES from './constants/routes';
 
 export default function App() {
-  return <p>hello</p>;
-  // return <Jumbotron />;
+  return (
+    <Router>
+      <Route exact path={ROUTES.BROUSE}>
+        <Browse />
+      </Route>
+
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
+
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
+  );
 }
---
+++ src/pages/browse.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Brouser() {
+  return <p>Hello from browser</p>;
+}
