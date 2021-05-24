15cc5ff5dd23baf9a1ac810db0bac35923c3391c
bharat2913 <bs987143@gmail.com>
2021-05-25 04:57:44 +0530

    style: change styles

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -13,13 +13,11 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 50px;
-
   > ${Title} {
     @media (max-width: 1000px) {
       margin-left: 30px;
     }
   }
-
   &:last-of-type {
     margin-bottom: 0;
   }
@@ -30,8 +28,7 @@ export const Group = styled.div`
   flex-direction: ${({ flexDirection }) =>
     flexDirection === 'row' ? 'row' : 'column'};
   ${({ alignItems }) => alignItems && `align-items: ${alignItems}`};
-  ${({ margin }) => margin && `margin: ${console.log(margin)}`};
-
+  ${({ margin }) => margin && `margin: ${margin}`};
   > ${Container}:first-of-type {
     @media (min-width: 1100px) {
       margin-top: -100px;
@@ -89,32 +86,26 @@ export const Item = styled.div`
   position: relative;
   cursor: pointer;
   transition: transform 0.2s;
-
   &:hover {
     transform: scale(1.3);
     z-index: 99;
   }
-
   @media (min-width: 1200px) {
     &:hover ${Meta}, &:hover ${Text}, &:hover ${SubTitle} {
       display: block;
       z-index: 100;
     }
   }
-
-  &:first-of-type: {
+  &:first-of-type {
     margin-left: 56px;
-
     @media (max-width: 1000px) {
       margin-left: 30px;
     }
   }
-
-  &:last-of-type: {
-    margin-left: 56px;
-
+  &:last-of-type {
+    margin-right: 56px;
     @media (max-width: 1000px) {
-      margin-left: 30px;
+      margin-right: 30px;
     }
   }
 `;
@@ -125,8 +116,7 @@ export const FeatureText = styled.p`
   font-weight: ${({ fontWeight }) =>
     fontWeight === 'bold' ? 'bold' : 'normal'};
   margin: 0;
-
-  @media (max-height: 600) {
+  @media (max-width: 600px) {
     line-height: 22px;
   }
 `;
@@ -141,17 +131,14 @@ export const Feature = styled.div`
   background-position-x: right;
   background-repeat: no-repeat;
   background-color: black;
-
   @media (max-width: 1000px) {
     height: auto;
     background-size: auto;
-
     ${Title} {
       font-size: 20px;
       line-height: 20px;
       margin-bottom: 10px;
     }
-
     ${FeatureText} {
       font-size: 14px;
     }
@@ -170,7 +157,6 @@ export const FeatureClose = styled.button`
   cursor: pointer;
   background-color: transparent;
   border: 0;
-
   img {
     filter: brightness(0) invert(1);
     width: 24px;
@@ -181,7 +167,6 @@ export const Content = styled.div`
   margin: 56px;
   max-width: 500px;
   line-height: normal;
-
   @media (max-width: 1000px) {
     margin: 30px;
     max-width: none;
@@ -193,7 +178,7 @@ export const Maturity = styled.div`
   border-radius: 15px;
   width: 28px;
   line-height: 28px;
-  text-align-items;
+  text-align: center;
   color: white;
   font-weight: bold;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
